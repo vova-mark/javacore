c7050ace24d10aa2a6f2e2fd0d54d8bccbbb5535
mark <vovamarkevich.br@gmail.com>
2017-11-26 21:13:31 +0200

    [Lesson4 - hometask] PrimitiveConvertor

---
+++ src/main/java/center/kit/app/homework/lesson4/utils/convertors/PrimitiveConvertor.java
@@ -0,0 +1,21 @@
+package center.kit.app.homework.lesson4.utils.convertors;
+
+public class PrimitiveConvertor {
+
+    //мне что то подсказывает, что не   мешало бы проверять передаваемые аргументы
+    //но не стал...
+
+    public char floatToChar(float value) {
+        return (char) value;
+    }
+
+    public char intToChar(int value) {
+        return (char) value;
+    }
+
+    public int charToInt(char value) {
+        return (int) value;
+    }
+}
+
+
---
+++ src/main/java/center/kit/runners/homework/lesson4/utils/convertors/Lesson4Runner.java
@@ -0,0 +1,19 @@
+package center.kit.runners.homework.lesson4.utils.convertors;
+
+import center.kit.app.homework.lesson4.utils.convertors.PrimitiveConvertor;
+
+public class Lesson4Runner {
+    public static void main(String[] args) {
+
+        int intValue = 33;
+        float floatValue = 2147483647.12f;
+        char charValue = '!';
+
+        PrimitiveConvertor convertor = new PrimitiveConvertor();
+
+        System.out.println("Input int value is: " + intValue + ". Output char value is: " + convertor.intToChar(intValue));
+        System.out.println("Input float value is: " + floatValue + ". Output char value is: " + convertor.floatToChar(floatValue));
+        System.out.println("Input char value is: " + charValue + ". Output int value is: " + convertor.charToInt(charValue));
+
+    }
+}
