98fce36f2b8198afd06ed1dc771036ddc13bda0d
mark <vovamarkevich.br@gmail.com>
2018-01-28 14:57:21 +0200

    TeamCityTestAdded

---
+++ src/test/java/center/kit/app/homework/lesson8/TestClass4TeamCity.java
@@ -0,0 +1,13 @@
+package center.kit.app.homework.lesson8;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestClass4TeamCity {
+
+    @Test
+    public void TeamCityTest(){
+        Assert.assertTrue(true);
+    }
+
+}
